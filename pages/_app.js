998c20258b6cd4385e71bbb75dfae11848a6e7a7
dr-stephen-achugwo <dr_stephen@aol.com>
2022-10-30 21:04:40 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    Aos.init();
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
